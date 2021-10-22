c9e444d48c5f07bd632742caa003de6b1c81ad32
Leonard Woo <leo_916@yahoo.com>
2021-10-22 17:41:38 +0800

    new ui 20211022 part 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 /* auto */
 "use strict";
 function getHeight(ele){var e=document.querySelector(ele);if(e==null){return 0}var h=e.clientHeight;if(h==null||h<0){h=0}return h};
-function calcMain(){let headerHeight=getHeight('header');let footerHeight=getHeight('footer');let main=document.querySelector('main');main.style.setProperty('min-height','calc(100vh - '+(headerHeight+footerHeight)+'px)');main.classList.add('flex')};
+function calcMain(){let main=document.querySelector('main');main.style.setProperty('min-height','calc(100vh - '+(getHeight('header')+getHeight('footer'))+'px)');main.classList.add('flex')};
 window.addEventListener('load',(event) => {calcMain();}, false);
