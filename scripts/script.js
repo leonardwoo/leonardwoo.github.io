4e68e979fd0f7b03719121d12f9b4f3c16f05bd1
Leonard Woo <leo_916@yahoo.com>
2022-09-26 10:19:33 +0800

    update work with pika-js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,3 @@
 /* auto */
 "use strict";
-function getHeight(ele){var e=document.querySelector(ele);if(e==null){return 0}var h=e.clientHeight;if(h==null||h<0){h=0}return h};
-function calcMain(){let main=document.querySelector('main');main.style.setProperty('min-height','calc(100vh - '+(getHeight('header')+getHeight('footer'))+'px)');main.classList.add('flex')};
-window.addEventListener('load',(event) => {calcMain();}, false);
+window.addEventListener('load',(event) => {CalcMinMain();});
