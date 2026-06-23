afadc7e76279b6f6e0cedab106beebf4c62f8379
Leonard Woo <leo_916@yahoo.com>
2026-06-23 15:03:34 +0800

    update astro 6.3, delete vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import sitemap from '@astrojs/sitemap';
 import { aonoteMarkdown } from './src/integrations/aonote-markdown.ts';
 import { site } from './src/site.config.ts';
 
-import vue from '@astrojs/vue';
-
 /** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
   site: site.baseUrl,
@@ -14,8 +12,7 @@ export default defineConfig({
   integrations: [aonoteMarkdown(), 
     sitemap({
       filter: (page) => !page.includes('/404'),
-    }), 
-    vue()
+    })
   ],
   vite: {
     build: {
